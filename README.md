1a77e039ff87dd706ee78c2ad212cba3d98cb8c6
YAT <yat@yat-net.com>
2017-02-03 15:13:54 +0900

    first

---
+++ README.md
@@ -0,0 +1,32 @@
+# themrish
+
+for WordPress theme develop auxiliary tool
+
+WordPressのテーマ開発補助ツールです。
+
+- ブラウザシンクによるソースファイル保存時のページリロード
+- sassコンパイラ
+- jsコンパイラ
+
+## 依存アプリケーション
+- [npm](https://www.npmjs.com/)
+- [gulp](http://gulpjs.com/)
+
+## install
+
+1. Download this theme
+2. Command execute
+ 1. ` npm install `
+ 2. edit /gulpconfig.js
+ 3. ` gulp `
+
+### 注意点
+config.default.js にて browserSync のproxyだけを変えたい場合は、このファイルに直接変更してください。
+
+カレントディレクトリにある gulpconfig.js にproxyだけを定義しようとすると、[files]の方を一緒に定義しないと、未定義で上書きされてしまいます。
+
+回避方法があったらどなたか教えてください。
+
+## version
+
+0.0.1 ベータリリース
---
+++ gulp/config.default.js
@@ -0,0 +1,48 @@
+export default  {
+
+	/**
+	 *
+	 * browserSync.
+	 *
+	 */
+	browserSync: {
+		// proxy: 'localhost:3000',
+		proxy: 'http://hsa.dev/', //自身のローカル環境に合わせて書き換えてください。
+		files: [
+			"./style.css",
+			"./js/bundle.js",
+			"./**/*.php",
+			"./**/*.png",
+			"./**/*.svg",
+			"./**/*.jpg"
+		]
+	},
+
+	/**
+	 *
+	 * sass Compile Option.
+	 *
+	 */
+	sass: {
+		src: './src/styles/**/*.{sass,scss}',
+		dest: './',
+		sourceRoot: './src/styles'
+	},
+
+	/**
+	 *
+	 * JavaScript.
+	 *
+	 */
+	browserify: {
+		bundleOption: {
+			cache: {}, packageCache: {}, fullPaths: false,
+			debug: true,
+			entries: './src/scripts/main.js',
+			extensions: [ 'js' ]
+		},
+		dest: './',
+		filename: 'js/bundle.js'
+
+	}
+};
